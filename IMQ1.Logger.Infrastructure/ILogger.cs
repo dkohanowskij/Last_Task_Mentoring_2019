32ac253cc96e623be96dd5e184169f3826a8c136
Dzmitriy <Dzmitriy_Kakhanouski@epam.com>
2019-05-28 10:56:22 +0300

    initial com

---
+++ IMQ1.Logger.Infrastructure/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IMQ1.Logger.Infrastructure
+{
+    public interface ILogger
+    {
+        void LogError(Exception exception, string message);
+
+        void LogInfo(string message);
+
+        void LogDebug(string message);
+    }
+}
---
+++ IMQ1.Logger/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NLog;
+
+namespace IMQ1.Logger
+{
+    public class Logger : Infrastructure.ILogger
+    {
+        private static NLog.ILogger logger;
+        public Logger()
+        {
+            logger = LogManager.GetLogger("Logger");
+        }
+
+        public void LogError(Exception exception, string message)
+        {
+            logger.Error(exception, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+    }
+}
